ded80cde88fc7ad88afb19c5a7f2864a445c7a4c
mdulac <melchior.dulac@inra.fr>
2019-12-18 18:41:06 +0100

    moved the testing to est

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python tool_rpOptBioDes.py -inputTar test_input.tar -outputTar test_output.tar -pathway_id rp_pathway -maxVariants 45 -libSize 128 -inputParts None
-
-mv test_output.tar results/
