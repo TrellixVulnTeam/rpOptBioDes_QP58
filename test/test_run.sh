287c1d9fcadd56fc8b8bce5a1c9d965fd1527fdb
joan <joan.herisson@univ-evry.fr>
2020-01-20 15:43:47 +0100

    rm test

--- test/test_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm -f test_output.tar
-docker run -d -p 8888:8888 --name test_rpOptBioDes brsynth/rpoptbiodes
-sleep 10
-python tool_rpOptBioDes.py -inputTar test_input.tar -outputTar test_output.tar -pathway_id rp_pathway -maxVariants 45 -libSize 102 -inputParts None -server_url http://0.0.0.0:8888/REST
-docker kill test_rpOptBioDes
-docker rm test_rpOptBioDes
