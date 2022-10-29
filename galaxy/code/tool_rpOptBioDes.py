e888e80e7a89c3533c618466dd3df8462b30d346
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-29 08:13:28 +0000

    Adding tarfile member sanitization to extractall()

--- galaxy/code/tool_rpOptBioDes.py
+++ galaxy/code/tool_rpOptBioDes.py
@@ -55,7 +55,29 @@ if __name__ == "__main__":
                                          params.lib_size,
                                          input_parts)
             with tarfile.open(output_tar) as outtar:
-                outtar.extractall(tmpoutputfolder)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(outtar, tmpoutputfolder)
             out_file = glob.glob(tmpoutputfolder+'/*.xml')
             if len(out_file)>1:
                 logging.warning('there are more than one output file...')
